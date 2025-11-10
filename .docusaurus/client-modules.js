fa7512ecf2eadd9a633b63851743d66d901d4b60
Your Name <2037934026@qq.com>
2025-11-10 21:06:28 +0800

    清理部署分支：移除 node_modules 和 .docusaurus

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("D:\\code\\MyProjects\\my-website\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
-  require("D:\\code\\MyProjects\\my-website\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("D:\\code\\MyProjects\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("D:\\code\\MyProjects\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("D:\\code\\MyProjects\\my-website\\src\\css\\custom.css"),
-];
