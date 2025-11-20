0565ef6da97305d225c50d765f1e2b866e663f84
Your Name <2037934026@qq.com>
2025-11-20 15:48:54 +0800

    更改网站标题

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: '歪歪踢',
+  tagline: '歪歪踢的网站',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
